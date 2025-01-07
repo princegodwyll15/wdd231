7e22f4dd454091d1b096350a7facca9c08db3eda
princegodwyll15 <godwyllprince@mail.com>
2025-01-07 14:24:33 +0000

    hamburger

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,13 @@
 const year =document.getElementById('year');
 const lastModified = document.getElementById("lastModified");
+//hamburger 
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('nav');
+hamburger.addEventListener('click', () => {
+    nav.classList.toggle('open');
+    hamburger.classList.toggle('open');
+
+});
 
 const currentDate = new Date();
 const getYear = currentDate.getFullYear();
